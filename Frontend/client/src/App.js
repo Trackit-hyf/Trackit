f430599593512e7f6404bb2e4bc950dd86e487ae
FarukTercan <trcnfaruk@gmail.com>
2020-08-20 14:21:34 +0200

    Homepage & Signup & Login components

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/layout/Home';
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
 import './App.css';
 
 function App() {
   return (
-    <Fragment>
-      <Navbar />
-      <Home />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 }
 
