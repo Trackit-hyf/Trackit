0d836a458682321299628bea3421447de28469f1
Ammar <zarosho@gmail.com>
2020-09-28 13:49:20 +0200

    fixing

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -5,7 +5,7 @@ const { validationResult } = require('express-validator');
 const User = require('../models/user');
 
 const signup = async (req, res, next) => {
-console.log("signup -> signup", hi)
+console.log("signup -> signup", 'hi')
 
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
@@ -62,7 +62,7 @@ console.log("signup -> signup", hi)
 };
 
 const login = async (req, res, next) => {
-    console.log("login -> login", hi login)
+    console.log("login -> login", 'hi login')
 	const { email, password } = req.body;
 
 
