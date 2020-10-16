0b225fc849b2ac70f9f2919a3ccd415ed5d13176
Ammar <zarosho@gmail.com>
2020-10-16 16:22:26 +0200

    made adding multiple assets possible and added sorting to assets

---
+++ Frontend/client/src/components/assets/AssetsCells.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Header, Table, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { calculate } from '../../utils/calculate';
+import formatDate from '../../utils/formatDate';
+
+export default function AssetsCells({ asset, onDelete }) {
+	const [ calculateTotalProfit, calculateTotal, styleGainAndLose, calculateProfit ] = calculate();
+
+	return (
+		<Table.Row key={asset.id}>
+			<Table.Cell width="2">
+				<Header as="h4">{asset.name}</Header>
+			</Table.Cell>
+			<Table.Cell width="2" textAlign="center">
+				{asset.price}
+			</Table.Cell>
+			<Table.Cell width="2" textAlign="center">
+				{asset.amount}
+			</Table.Cell>
+
+			<Table.Cell width="2" textAlign="center">
+				{formatDate(asset.dateOfPurchase)}
+			</Table.Cell>
+			<Table.Cell width="2" textAlign="center">
+				{asset.hourly_price.length > 1 ? (
+					styleGainAndLose(
+						asset.hourly_price[asset.hourly_price.length - 1].price,
+						asset.hourly_price[asset.hourly_price.length - 2].price
+					)
+				) : (
+					<p className="priceNoChange">{asset.hourly_price[0] && asset.hourly_price[0].price}</p>
+				)}
+			</Table.Cell>
+			<Table.Cell width="2" textAlign="center">
+				{asset.hourly_price && calculateProfit(asset.price, asset)}
+			</Table.Cell>
+			<Table.Cell
+				width="2"
+				textAlign="center"
+				style={{
+					textDecoration: 'underline',
+					color: 'orangered'
+				}}
+			>
+				{asset.hourly_price.length > 1 && (
+					<Link
+						to={{
+							pathname: '/myassets/chart',
+							state: { asset }
+						}}
+					>
+						Price late changes
+					</Link>
+				)}
+			</Table.Cell>
+			<Table.Cell width="1" textAlign="center">
+				<Button circular icon="delete" size="mini" inverted color="red" onClick={() => onDelete(asset._id)} />
+			</Table.Cell>
+		</Table.Row>
+	);
+}
--- Frontend/client/src/utils/calculate.js
+++ Frontend/client/src/utils/calculate.js
@@ -1,5 +1,6 @@
+import React from 'react';
+
 export const calculate = () => {
-	
 	const calculateTotalProfit = (assets) => {
 		if (assets) {
 			const totalProfitArr = assets.map(
@@ -20,5 +21,25 @@ export const calculate = () => {
 			return totalAsset;
 		}
 	};
-	return [  calculateTotalProfit, calculateTotal ];
+
+	const styleGainAndLose = (priceA, priceB) => {
+		if (priceA > priceB) {
+			return <p className="priceIncrease "> &#x2B9D; {priceA}</p>;
+		} else if (priceA < priceB) {
+			return <p className="priceDecrease "> &#x2B9F; {priceA}</p>;
+		} else {
+			return <p className="priceNoChange "> {priceA}</p>;
+		}
+	};
+	const calculateProfit = (purchasePrice, asset) => {
+		const amount = asset.amount;
+		const currentPrice = asset.hourly_price[asset.hourly_price.length - 1].price;
+		const difference = (currentPrice - purchasePrice) * amount;
+		return difference < 0 ? (
+			<p className="priceDecrease"> {difference.toFixed(3)}</p>
+		) : (
+			<p className="priceIncrease">{difference.toFixed(3)}</p>
+		);
+	};
+	return [ calculateTotalProfit, calculateTotal, styleGainAndLose, calculateProfit ];
 };
